432d4cf999f9be4af0281312e5f5d01d7f7cab29
Pranay <pranay1315@gmail.com>
2020-08-19 12:46:00 +0530

    Changed FontFamily to Arvo,added FA icons

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -5,7 +5,7 @@ const Recipe = ({ title, calories, image }) => {
 		<div>
 			<h1>{title}</h1>
 			<p>{calories}</p>
-			<img src={image} alt={title} />
+			<img src={image} alt={title} className="recipe-img" />
 		</div>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -33,70 +33,100 @@ class App extends Component {
 		console.log("Submitted");
 		this.fetchData().catch((err) => {
 			if (err) {
-				document.querySelector("#message-container").innerHTML = "Error";
+				document.querySelector("#spinner-container").innerHTML = "Error";
 				console.log(err);
 			}
 		});
 	};
 	fetchData = async () => {
-		document.querySelector("#message-container").innerHTML = "Loading...";
+		document.querySelector("#spinner-container").innerHTML = "Loading...";
 		const response = await fetch(
 			`https://api.edamam.com/search?q=${
 				this.state.query || "chicken"
 			}&app_id=${this.APP_ID}&app_key=${this.API_KEY}`
 		);
 		const data = await response.json();
-		document.querySelector("#message-container").innerHTML = "";
+		document.querySelector("#spinner-container").innerHTML = "";
 		this.setState({ dataArray: data.hits });
+		console.log(data.hits);
 	};
 	render() {
 		return (
 			<div className="App">
-				<form className="search-form" onSubmit={this.handleChange}>
-					<input
-						className="search-bar"
-						type="text"
-						value={this.state.query}
-						onChange={(e) => {
-							this.setState({ query: e.target.value });
-						}}
-					/>
-					<span
-						style={{
-							background: "none",
-							border: "none",
-							position: "absolute",
-							left: "145px",
-							outline: "none",
-							cursor: "pointer",
-						}}
-						onClick={(e) => {
-							e.preventDefault();
-							console.log("cleared");
-							this.setState({ query: "" });
-						}}
-						id="clear-button"
-					>
-						x
-					</span>
-
-					<button className="search-button" type="submit">
-						Search
-					</button>
-					<div id="recipe-container">
-						<div id="message-container"></div>
-						{this.state.dataArray.map(
-							({ recipe: { label, calories, image } }, index) => (
-								<Recipe
-									key={index}
-									title={label}
-									calories={calories}
-									image={image}
-								/>
-							)
-						)}
-					</div>
-				</form>
+				<div className="form-container">
+					<form className="search-form" onSubmit={this.handleChange}>
+						<div
+							style={{
+								position: "relative",
+							}}
+							className="btn-container"
+						>
+							<input
+								className="search-bar"
+								type="text"
+								value={this.state.query}
+								onChange={(e) => {
+									this.setState({ query: e.target.value });
+								}}
+								style={{
+									width: "100%",
+									padding: "10px 5px",
+									borderRadius: "5px",
+									border: "none",
+									outline: "none",
+									fontWeight: "bold",
+									boxShadow: "1px 1px 8px 1px grey",
+								}}
+							/>
+							<span
+								style={{
+									background: "none",
+									border: "none",
+									position: "absolute",
+									right: "5px",
+									top: "25%",
+									outline: "none",
+									cursor: "pointer",
+								}}
+								onClick={(e) => {
+									e.preventDefault();
+									console.log("cleared");
+									this.setState({ query: "" });
+								}}
+								id="clear-button"
+							>
+								<i className="fa fa-times"></i>
+							</span>
+							<span
+								style={{
+									position: "absolute",
+									right: "-25px",
+									top: "20%",
+									cursor: "pointer",
+								}}
+							>
+								<i
+									className="fa fa-search"
+									aria-hidden="true"
+									onClick={this.handleChange}
+								></i>
+							</span>
+						</div>
+					</form>
+				</div>
+				<div id="recipe-container">
+					<div id="spinner-container"></div>
+					{this.state.dataArray.map(
+						({ recipe: { label, calories, image } }, index) => (
+							<Recipe
+								key={index}
+								title={label}
+								calories={calories}
+								image={image}
+							/>
+						)
+					)}
+				</div>
 			</div>
 		);
 	}
